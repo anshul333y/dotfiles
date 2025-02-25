fe709a25515ade955b281a96ba670b5c9563ac41
Anshul Yadav <anshul333y@gmail.com>
2025-02-25 15:16:56 +0530

    added fonts-install.sh & bin

---
+++ bin/.local/bin/fonts-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Download the font zip file
+wget -P ~/Downloads "https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/CascadiaCode.zip"
+
+# Unzip the font file
+unzip ~/Downloads/CascadiaCode.zip -d ~/Downloads/fonts
+
+# Move the font files to the system fonts directory
+mv ~/Downloads/fonts ~/.local/share
+
+# Update the font cache
+fc-cache -f -v
+
+# Clean the zip file
+rm ~/Downloads/CascadiaCode.zip
+
+echo "Fonts installed successfully!"
