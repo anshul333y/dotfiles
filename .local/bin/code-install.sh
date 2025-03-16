beb63bbf9e0a854d1b0266bb6072e052c16f135f
Anshul Yadav <anshul333y@gmail.com>
2025-03-16 22:03:11 +0530

    modified bin

--- .local/bin/code-install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wget -O ~/Downloads/code.deb "https://code.visualstudio.com/sha/download?build=stable&os=linux-deb-x64"
-
-sudo apt install ~/Downloads/code.deb
-
-rm ~/Downloads/code.deb
-
-echo "code installed successfully!"
